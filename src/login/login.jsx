245cddbe7c847606355fb7128775c61cba01398f
hamdhanvkr <hamdhanvkr@gmail.com>
2024-12-03 23:40:02 +0530

    login page setup

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,29 +1,50 @@
 import React from 'react'
 
 import Logo from '../assets/upvc_logo.jpeg'
+import { useNavigate } from 'react-router-dom'
 
 const login = () => {
+    const navigate = useNavigate()
+
+    const handleLogin = () =>{
+        navigate('/layout')
+    }
+    
+
     return (
-        <div className='bg-blue-500 h-screen w-screen flex flex-row justify-center items-center '>
-            <div className='bg-white w-2/3 h-2/3 rounded-2xl flex flex-row gap-32 justify-center items-center'>
-                <div className='bg-blue-200 w-2/5 h-4/5 '>
-                    <img src={Logo} className='w-full h-full' />
+        <div className="bg-gray-200 h-screen w-screen flex flex-row justify-center items-center">
+            <div className="bg-teal-700 w-3/4 h-3/4 rounded-2xl flex flex-row justify-center items-center gap-20">
+                <div className="w-2/5 h-4/4">
+                    <img src={Logo} className="w-full h-96 rounded-xl" />
                 </div>
-                <div className='h-96 w-96 bg-red-200'>
-                    <div className='flex flex-col'>
-                    <span>Username</span>
-                    <input type="text" className='' placeholder='Username' />
-                    <span>Password</span>
-                    <input type="text" className='' placeholder='Password' />
+                <div className="h-96 w-auto flex flex-row justify-center items-center">
+                    <div className="flex flex-col gap-10">
+                        <div className="flex flex-row gap-3">
+                            <span className="font-bold flex flex-row items-center text-gray-200">USERNAME:</span>
+                            <input
+                                type="text"
+                                className="w-80 p-3 rounded-xl border border-black-400"
+                                placeholder="Username"
+                            />
+                        </div>
+                        <div className="flex flex-row gap-3">
+                            <span className="font-bold flex flex-row items-center text-gray-200">PASSWORD:</span>
+                            <input
+                                type="password"
+                                className="w-80 p-3 rounded-xl border border-black-400"
+                                placeholder="Password"
+                            />
+                        </div>
+                        <div className="flex flex-row justify-center mt-5">
+                            <button className="w-80 ml-24 p-3 bg-orange-500 rounded-xl text-white font-bold hover:bg-orange-600"
+                            onClick={handleLogin}>
+                                LOGIN
+                            </button>
+                        </div>
                     </div>
                 </div>
-
-
             </div>
-
-
         </div>
-
 
     )
 }
