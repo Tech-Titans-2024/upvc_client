27d38a20582a8004fb4dae9f3843e1395eef3dbd
hamdhanvkr <hamdhanvkr@gmail.com>
2024-12-27 22:02:46 +0530

    product images setup

--- src/components/manage/manage.jsx
+++ src/components/manage/manage.jsx
@@ -23,10 +23,10 @@ function App() {
                 setIsValidTypeId(true);
             } else {
                 setIsValidTypeId(false);
-                alert('Type ID does not exist in the database !!')
+                alert('Type ID does not exist in the database!');
             }
         } catch (error) {
-            console.error('Error checking typeId:', error);
+            console.error('Error checking Type ID:', error);
             setIsValidTypeId(false);
         }
     };
@@ -61,28 +61,30 @@ function App() {
     };
 
     return (
-        <div className='bg-blue-300 h-full rounded-lg'>
-            <h1 className='text-black text-3xl font-bold text-center'>Upload Product Image</h1>
-            <div className='p-5'>
-                <div className='flex justify-center items-center gap-5 mt-10 border-2 border-black h-60 rounded-lg'>
-                    <label className='font-bold text-lg'>Type Id: </label>
+        <div className="bg-blue-300 h-full rounded-lg">
+            <h1 className="text-black text-3xl font-bold text-center">Upload Product Image</h1>
+            <div className="p-5">
+                <div className="flex justify-center items-center gap-5 mt-10 border-2 border-black h-60 rounded-lg">
+                    <label className="font-bold text-lg">Type ID:</label>
                     <input
                         type="number"
-                        className='w-40 h-12 p-3 border-2 border-black rounded-lg  placeholder-gray-400'
+                        className="w-40 h-12 p-3 border-2 border-black rounded-lg placeholder-gray-400"
                         value={typeId}
                         onChange={handleTypeIdChange}
                         onBlur={checkTypeIdExists}
                         placeholder="Enter Type ID"
                     />
-                <div className='flex gap-5'>
-                    <div className='border-2 border-black rounded-lg  bg-white h-12 p-2 w-80 '>
-                        <input type="file" onChange={handleFileChange} />
+                    <div className="flex gap-5">
+                        <div className="border-2 border-black rounded-lg bg-white h-12 p-2 w-80">
+                            <input type="file" onChange={handleFileChange} />
+                        </div>
+                        <button
+                            className="bg-blue-700 w-32 h-12 font-bold text-lg text-white py-2.5 px-6 rounded-lg shadow hover:bg-blue-800 transition duration-200"
+                            onClick={handleUpload}
+                        >
+                            Upload
+                        </button>
                     </div>
-                    <button
-                        className="bg-blue-700 w-32 h-12 font-bold text-lg text-white py-2.5 px-6 rounded-lg shadow hover:bg-blue-800 transition duration-200"
-
-                        onClick={handleUpload}>Upload</button>
-                </div>
                 </div>
             </div>
         </div>
